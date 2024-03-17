f49d6bae1c5906ac030c6913603f0b7d3d43095c
Egor Melnichyk <egor.mel8@gmail.com>
2024-03-17 23:08:16 +0300

    New gradle

--- FirstTest/src/test/java/FirstTestForm.java
+++ FirstTest/src/test/java/FirstTestForm.java
@@ -1,4 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FirstTestForm {
-
-
+    @Test
+    void AutoInputForm () {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Egor");
+    }
 }
